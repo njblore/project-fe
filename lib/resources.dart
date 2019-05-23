31faee9ac1a99b30a04932b2592cd117f8d8da78
njblore <njblore@outlook.com>
2019-05-23 15:40:11 +0100

    add scroll view to reseources page

--- lib/resources.dart
+++ lib/resources.dart
@@ -36,126 +36,132 @@ class Resources extends StatelessWidget {
             child: Container(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                        elevation: 10.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(11.0),
-                          child: Column(
-                            children: <Widget>[
-                              Text("Emergency:",
-                                  style: Theme.of(context).textTheme.title,
-                                  textScaleFactor: .7),
-                              Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    FlatButton(
-                                        child: Text("Samaritans"),
-                                        onPressed: () {
-                                          launchURL(
-                                              'https://www.samaritans.org/');
-                                        }),
-                                    FlatButton(
-                                      child: Text("Phone: 116 123"),
-                                      onPressed: () {
-                                        launch("tel://116123");
-                                      },
-                                    )
-                                  ]),
-                              FlatButton(
-                                child: Text("NHS: Help for Suicidal Thougts"),
-                                onPressed: () {
-                                  launchURL(
-                                      'https://www.nhs.uk/conditions/suicide/');
-                                },
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                        elevation: 10.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(11.0),
-                          child: Column(children: <Widget>[
-                            Text("Mental Health:",
-                                style: Theme.of(context).textTheme.title,
-                                textScaleFactor: .7),
-                            Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  FlatButton(
-                                    child: Text("Mind"),
-                                    onPressed: () {
-                                      launchURL('https://www.mind.org.uk/');
-                                    },
-                                  ),
-                                  FlatButton(
-                                    child: Text("Phone: 0300 123 3393"),
-                                    onPressed: () {
-                                      launch("tel://03001233393");
-                                    },
-                                  )
-                                ]),
-                            FlatButton(
-                                child: Text("Mental Health Foundation"),
-                                onPressed: () {
-                                  launchURL('https://www.mentalhealth.org.uk/');
-                                }),
-                            FlatButton(
-                              child: Text("NHS: Find Services Near You"),
-                              onPressed: () {
-                                launchURL('https://www.nhs.uk/service-search/');
-                              },
-                            ),
-                          ]),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
                           elevation: 10.0,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(15.0)),
                           child: Padding(
                             padding: const EdgeInsets.all(11.0),
                             child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: <Widget>[
-                                  Text("Wellbeing:",
-                                      style: Theme.of(context).textTheme.title,
-                                      textAlign: TextAlign.center,
-                                      textScaleFactor: .7),
-                                  FlatButton(
-                                    child: Text("NHS: Mindfulness"),
-                                    onPressed: () {
-                                      launchURL(
-                                          'https://www.nhs.uk/conditions/stress-anxiety-depression/mindfulness/');
-                                    },
-                                  ),
-                                  FlatButton(
-                                    child: Text(
-                                        "Mindful: Getting Started with Mindfulness"),
-                                    onPressed: () {
-                                      launchURL(
-                                          'https://www.mindful.org/meditation/mindfulness-getting-started/');
-                                    },
-                                  ),
-                                ]),
-                          )),
-                    ),
-                  ],
+                              children: <Widget>[
+                                Text("Emergency:",
+                                    style: Theme.of(context).textTheme.title,
+                                    textScaleFactor: .7),
+                                Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      FlatButton(
+                                          child: Text("Samaritans"),
+                                          onPressed: () {
+                                            launchURL(
+                                                'https://www.samaritans.org/');
+                                          }),
+                                      FlatButton(
+                                        child: Text("Phone: 116 123"),
+                                        onPressed: () {
+                                          launch("tel://116123");
+                                        },
+                                      )
+                                    ]),
+                                FlatButton(
+                                  child: Text("NHS: Help for Suicidal Thougts"),
+                                  onPressed: () {
+                                    launchURL(
+                                        'https://www.nhs.uk/conditions/suicide/');
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          elevation: 10.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(11.0),
+                            child: Column(children: <Widget>[
+                              Text("Mental Health:",
+                                  style: Theme.of(context).textTheme.title,
+                                  textScaleFactor: .7),
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    FlatButton(
+                                      child: Text("Mind"),
+                                      onPressed: () {
+                                        launchURL('https://www.mind.org.uk/');
+                                      },
+                                    ),
+                                    FlatButton(
+                                      child: Text("Phone: 0300 123 3393"),
+                                      onPressed: () {
+                                        launch("tel://03001233393");
+                                      },
+                                    )
+                                  ]),
+                              FlatButton(
+                                  child: Text("Mental Health Foundation"),
+                                  onPressed: () {
+                                    launchURL(
+                                        'https://www.mentalhealth.org.uk/');
+                                  }),
+                              FlatButton(
+                                child: Text("NHS: Find Services Near You"),
+                                onPressed: () {
+                                  launchURL(
+                                      'https://www.nhs.uk/service-search/');
+                                },
+                              ),
+                            ]),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                            elevation: 10.0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(11.0),
+                              child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: <Widget>[
+                                    Text("Wellbeing:",
+                                        style:
+                                            Theme.of(context).textTheme.title,
+                                        textAlign: TextAlign.center,
+                                        textScaleFactor: .7),
+                                    FlatButton(
+                                      child: Text("NHS: Mindfulness"),
+                                      onPressed: () {
+                                        launchURL(
+                                            'https://www.nhs.uk/conditions/stress-anxiety-depression/mindfulness/');
+                                      },
+                                    ),
+                                    FlatButton(
+                                      child: Text(
+                                          "Mindful: Getting Started with Mindfulness"),
+                                      onPressed: () {
+                                        launchURL(
+                                            'https://www.mindful.org/meditation/mindfulness-getting-started/');
+                                      },
+                                    ),
+                                  ]),
+                            )),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
